285fbfc135d879ee46be1e3e3c96ce36664b46d9
Pathum12345678 <108787698+Pathum12345678@users.noreply.github.com>
2023-03-20 15:51:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,17 +20,17 @@ global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
 global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
 global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0770531536'] //ur owner number
+global.owner = ['0717777997'] //ur owner number
 global.ownername = "pathum" //ur owner name
 global.ytname = "YT: Dark" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "UVJSOFZHOFQ=" //session Id
-global.ownernomer = "94770531536" //ur number
+global.session = "YWlBaHA5aDc= " //session Id
+global.ownernomer = "94717777997" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94770531536'] //ur premium number
+global.premium = ['94717777997'] //ur premium number
 global.botname = 'ᴘᴀᴛʜᴜᴍ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
